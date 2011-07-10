5461658d800667fbb3d5311a6b8fa9f5eced5c81
Janusz Krzysztofik <jkrzyszt@tis.icnet.pl>
2011-07-10 19:13:49 +0200

    Drop millisleep(10) from xread() not only on Darwin
    Commit 6352911c20, "Fix selected Heyu commands waiting forever on
    Darwin", removed a millisleep(10) call from internal loop of xread()
    when built for Darwin. Since there were no reports about more excessive
    load then before on Darwin after this change, and it looks like the
    spool file, from which the xread() is fetching data, is always open in
    blocking mode, preventing read() calls from returning immediately if no
    data and the xread() internal loop from spinning, let's try dropping
    that millisleep(10) completely.
    
    Created against Heyu 2.10-rc1.
    Tested on linux.
    
    Signed-off-by: Janusz Krzysztofik <jkrzyszt@tis.icnet.pl>

--- xread.c
+++ xread.c
@@ -136,9 +136,6 @@ unsigned char *buf;
 		{
 		    break;
 		}
-#ifndef DARWIN
-                millisleep(10);  /* Added by CWS */
-#endif
 		continue;
 	    }
 	    if( (i < 0) && (i_am_relay != 1) )
