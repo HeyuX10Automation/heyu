56a8969c877d2c449573141bf4b83a271120eb7a
Janusz Krzysztofik <jkrzyszt@tis.icnet.pl>
2011-08-07 12:37:53 +0200

    Heyu 2.10-rc2
    Signed-off-by: Janusz Krzysztofik <jkrzyszt@tis.icnet.pl>

--- version.h
+++ version.h
@@ -2,5 +2,5 @@
  * It should be manually changed when the version number is ready to be
  * bumped up.
  */
-#define VERSION "2.10-rc1"
-#define RELEASE_DATE "20110227"
+#define VERSION "2.10-rc2"
+#define RELEASE_DATE "20110807"
